2a5b4a2fa702fd2065e7ccc9b669e7e825452b80
josdem <joseluis.delacruz@gmail.com>
2019-05-17 15:06:38 -0400

    Adding exception handler

--- src/main/java/com/jos/dem/springboot/uri/encoding/controller/EncoderController.java
+++ src/main/java/com/jos/dem/springboot/uri/encoding/controller/EncoderController.java
@@ -2,11 +2,15 @@ package com.jos.dem.springboot.uri.encoding.controller;
 
 import reactor.core.publisher.Mono;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import javax.validation.constraints.Pattern;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.validation.annotation.Validated;
 
 import org.slf4j.Logger;
@@ -22,10 +26,17 @@ public class EncoderController {
   public Mono<String> index(
       @PathVariable
         @Pattern(
-          regexp = "^\\d{4}[-]\\d{2}[-]\\d{2}[-]\\d+$",
+          regexp = "^\\d{4}[-]\\d{2}[-]\\d{2}[#|-]\\d+$",
           message = "Invalid storeOrderId format") String storeOrderId) {
     log.info("Calling to index");
     return Mono.just("Hello World!");
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  Mono<String> handleException(ConstraintViolationException cve){
+    log.info("Violations: " + ToStringBuilder.reflectionToString(cve.getConstraintViolations()));
+    cve.getConstraintViolations().forEach(v -> log.info(ToStringBuilder.reflectionToString(v)));
+    return Mono.just(cve.getMessage());
   }
 
 }
