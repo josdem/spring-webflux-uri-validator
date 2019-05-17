f53a95ef0b2b007b9e0e46509e11e65dcc94f165
josdem <joseluis.delacruz@gmail.com>
2019-05-17 08:28:43 -0400

    Starting new Spring Boot URI encoder with Webflux :)

---
+++ src/main/java/com/jos/dem/springboot/uri/encoding/UriEncoderApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.springboot.uri.encoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UriEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UriEncoderApplication.class, args);
+	}
+
+}
