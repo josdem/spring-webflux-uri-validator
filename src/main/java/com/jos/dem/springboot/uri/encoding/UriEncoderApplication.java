3e91c1596971dd797ac27586dcc8cd1e64200448
josdem <joseluis.delacruz@gmail.com>
2019-05-17 10:10:11 -0400

    Adding Webclient bean

--- src/main/java/com/jos/dem/springboot/uri/encoding/UriEncoderApplication.java
+++ src/main/java/com/jos/dem/springboot/uri/encoding/UriEncoderApplication.java
@@ -2,6 +2,8 @@ package com.jos.dem.springboot.uri.encoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class UriEncoderApplication {
@@ -9,5 +11,10 @@ public class UriEncoderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UriEncoderApplication.class, args);
 	}
+
+  @Bean
+  WebClient webClient() {
+    return WebClient.create("http://localhost:8080");
+  }
 
 }
--- src/main/java/com/jos/dem/springboot/uri/encoding/controller/EncoderController.java
+++ src/main/java/com/jos/dem/springboot/uri/encoding/controller/EncoderController.java
@@ -1,5 +1,7 @@
 package com.jos.dem.springboot.uri.encoding.controller;
 
+import reactor.core.publisher.Mono;
+
 import javax.validation.constraints.Pattern;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +19,13 @@ public class EncoderController {
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @GetMapping("/{storeOrderId}")
-  public String index(
+  public Mono<String> index(
       @PathVariable
         @Pattern(
           regexp = "^\\d{4}[-]\\d{2}[-]\\d{2}[#]\\d+$",
           message = "Invalid storeOrderId format") String storeOrderId) {
     log.info("Calling to index");
-    return "Hello World!";
+    return Mono.just("Hello World!");
   }
 
 }
